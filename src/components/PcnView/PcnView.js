69a600fb8f6155fc8797bf4d6a855f63b7deaefb
Mitch Olson <mitchjolson@gmail.com>
2019-09-03 12:14:30 -0500

    added PcnView component

---
+++ src/components/PcnView/PcnView.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class PcnView extends Component {
+
+    componentDidMount() {
+        this.props.dispatch({ type: 'FETCH_NOTES' });
+    }
+
+    handleChange(value) {
+        this.setState({ text: value })
+        console.log(this.state);
+    }
+
+    handleSubmit() {
+        this.props.dispatch({ type: 'ADD_NOTE', payload: this.state })
+    }
+
+
+    render() {
+        return (
+
+            <>
+                <div>
+                    <h2>React Quill - Rich Text</h2>
+                    <br />
+                    <h4>Add a note</h4>
+                    <ReactQuill className="what" theme="snow" value={this.state.text}
+                        onChange={this.handleChange} />
+                    <button onClick={() => this.handleSubmit()}>Submit</button>
+                </div>
+                <div className='notesDiv'>
+                    {this.props.reduxStore.notes.map((note, i) => {
+                        return (<div className="note" key={i} dangerouslySetInnerHTML={{
+                            __html:
+                                note.text
+                        }}></div>);
+                    })}
+                </div>
+            </>
+        )
+    }
+}
+
+const mapStateToProps = (reduxStore) => ({
+    reduxStore
+});
+
+export default connect(mapStateToProps)(PcnView);
