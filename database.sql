f6e4d580a014247543cc5a834b185c25f5248bef
David Reeves <davidsreeves1@gmail.com>
2019-09-03 10:59:25 -0500

    added database set-up

--- database.sql
+++ database.sql
@@ -1,10 +1,104 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+  "id" SERIALPRIMARYKEY,,
+  "username" VARCHAR(200),,
+  "password" VARCHAR(200),,
+  "admin" INT
+);
+
+CREATE TABLE eol (
+    id SERIAL PRIMARY KEY,
+    type character varying(200),
+    date date,
+    number integer,
+    change_description character varying(2000),
+    contact_id integer REFERENCES user(id),
+    end_dates character varying(200),
+    notes character varying(200),
+    audience character varying(1000),
+    images character varying(1000),
+    creator_id integer REFERENCES user(id)
+);
+
+CREATE TABLE eol_image (
+    id SERIAL PRIMARY KEY,
+    eol_id integer REFERENCES eol(id),
+    image_id integer REFERENCES image(id)
+);
+
+CREATE TABLE eol_part (
+    id SERIAL PRIMARY KEY,
+    eol_id integer REFERENCES eol(id),
+    part_id integer REFERENCES part(id),
+    replacement boolean
+);
+
+CREATE TABLE image (
+    id SERIAL PRIMARY KEY,
+    image_url character varying(1000),
+    figure character varying(200)
+);
+
+CREATE TABLE notifications (
+    id SERIAL PRIMARY KEY,
+    message character varying(2000)
+);
+
+CREATE TABLE npi (
+    id SERIAL PRIMARY KEY,
+    type character varying(200),
+    date date,
+    number integer,
+    description character varying(2000),
+    contact_id integer REFERENCES user(id),
+    notes character varying(2000),
+    creator_id integer REFERENCES user(id)
+);
+
+CREATE TABLE npi_image (
+    id SERIAL PRIMARY KEY,
+    npi_id integer REFERENCES npi(id),
+    image_id integer REFERENCES image(id)
+);
+
+CREATE TABLE npi_part (
+    id SERIAL PRIMARY KEY,
+    npi_id integer REFERENCES npi(id),
+    part_id integer REFERENCES part(id)
+);
+
+CREATE TABLE part (
+    id SERIAL PRIMARY KEY,
+    name character varying(200),
+    number character varying(200),
+    description character varying(200)
+);
+
+CREATE TABLE pcn (
+    id SERIAL PRIMARY KEY,
+    creator_id integer REFERENCES user(id),
+    type character varying(200),
+    date date,
+    number integer,
+    change_description character varying(2000),
+    contact_id integer REFERENCES user(id),
+    notes character varying(2000)
+);
+
+CREATE TABLE pcn_part (
+    id SERIAL PRIMARY KEY,
+    pcn_id integer REFERENCES pcn(id),
+    part_id integer REFERENCES part(id)
+);
+
+CREATE TABLE user (
+    id SERIAL PRIMARY KEY,
+    username character varying(80) NOT NULL UNIQUE,
+    password character varying(1000) NOT NULL,
+    admin integer
+);
+
+CREATE TABLE user_notification (
+    id SERIAL PRIMARY KEY,
+    user_id integer REFERENCES user(id),
+    notification_id integer REFERENCES notifications(id)
 );
