dbc1ba6c1f1ee7ef4ce0743fb1b241313c9ca1d1
Mitch Olson <mitchjolson@gmail.com>
2019-09-03 11:31:42 -0500

    updated database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "user" (
-  "id" SERIALPRIMARYKEY,,
-  "username" VARCHAR(200),,
-  "password" VARCHAR(200),,
+  "id" SERIAL PRIMARY KEY,
+  "username" VARCHAR(200),
+  "password" VARCHAR(200),
   "admin" INT
 );
 
@@ -19,30 +19,6 @@ CREATE TABLE eol (
     creator_id integer REFERENCES "user"(id)
 );
 
-CREATE TABLE eol_image (
-    id SERIAL PRIMARY KEY,
-    eol_id integer REFERENCES eol(id),
-    image_id integer REFERENCES image(id)
-);
-
-CREATE TABLE eol_part (
-    id SERIAL PRIMARY KEY,
-    eol_id integer REFERENCES eol(id),
-    part_id integer REFERENCES part(id),
-    replacement boolean
-);
-
-CREATE TABLE image (
-    id SERIAL PRIMARY KEY,
-    image_url character varying(1000),
-    figure character varying(200)
-);
-
-CREATE TABLE notifications (
-    id SERIAL PRIMARY KEY,
-    "message" character varying(2000)
-);
-
 CREATE TABLE npi (
     id SERIAL PRIMARY KEY,
     "type" character varying(200),
@@ -52,25 +28,6 @@ CREATE TABLE npi (
     contact_id integer REFERENCES "user"(id),
     notes character varying(2000),
     creator_id integer REFERENCES "user"(id)
-);
-
-CREATE TABLE npi_image (
-    id SERIAL PRIMARY KEY,
-    npi_id integer REFERENCES npi(id),
-    image_id integer REFERENCES "image"(id)
-);
-
-CREATE TABLE npi_part (
-    id SERIAL PRIMARY KEY,
-    npi_id integer REFERENCES npi(id),
-    part_id integer REFERENCES part(id)
-);
-
-CREATE TABLE part (
-    id SERIAL PRIMARY KEY,
-    "name" character varying(200),
-    "number" character varying(200),
-    "description" character varying(200)
 );
 
 CREATE TABLE pcn (
@@ -84,10 +41,53 @@ CREATE TABLE pcn (
     notes character varying(2000)
 );
 
+CREATE TABLE image (
+    id SERIAL PRIMARY KEY,
+    image_url character varying(1000),
+    figure character varying(200)
+);
+
+CREATE TABLE eol_image (
+    id SERIAL PRIMARY KEY,
+    eol_id integer REFERENCES eol(id),
+    image_id integer REFERENCES image(id)
+);
+
+CREATE TABLE npi_image (
+    id SERIAL PRIMARY KEY,
+    npi_id integer REFERENCES npi(id),
+    image_id integer REFERENCES "image"(id)
+);
+
+CREATE TABLE part (
+    id SERIAL PRIMARY KEY,
+    "name" character varying(200),
+    "number" character varying(200),
+    "description" character varying(200)
+);
+
+CREATE TABLE eol_part (
+    id SERIAL PRIMARY KEY,
+    eol_id integer REFERENCES eol(id),
+    part_id integer REFERENCES part(id),
+    replacement boolean
+);
+
+CREATE TABLE npi_part (
+    id SERIAL PRIMARY KEY,
+    npi_id integer REFERENCES npi(id),
+    part_id integer REFERENCES part(id)
+);
+
 CREATE TABLE pcn_part (
     id SERIAL PRIMARY KEY,
     pcn_id integer REFERENCES pcn(id),
     part_id integer REFERENCES part(id)
+);
+
+CREATE TABLE notifications (
+    id SERIAL PRIMARY KEY,
+    "message" character varying(2000)
 );
 
 CREATE TABLE user_notification (
