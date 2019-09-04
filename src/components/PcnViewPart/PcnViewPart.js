236fb4d40594b7269bde95908a44a1a5c7404daa
Mitch Olson <mitchjolson@gmail.com>
2019-09-04 13:30:39 -0500

    PCN fields rendering to pcn view component

---
+++ src/components/PcnViewPart/PcnViewPart.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// Material UI
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const CustomTableCell = withStyles(theme => ({
+    head: {
+        backgroundColor: theme.palette.common.white,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    },
+    input: {
+        display: 'none',
+    },
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 300,
+    },
+    row: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default,
+        },
+    },
+});
+
+class PcnViewPart extends Component {
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <>
+                <TableRow>
+                    <CustomTableCell>{this.props.part.number}</CustomTableCell>
+                    <CustomTableCell>{this.props.part.name}</CustomTableCell>
+                    <CustomTableCell>{this.props.part.description}</CustomTableCell>
+                </TableRow>
+            </>
+        );
+    }
+}
+
+const mapStateToProps = (reduxStore) => ({
+    reduxStore
+})
+
+export default connect(mapStateToProps)(withStyles(styles)(PcnViewPart));
