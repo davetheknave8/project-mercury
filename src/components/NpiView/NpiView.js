9c44e7916b5c5ff81e7a7a506a19c5b372e65b27
Mitch Olson <mitchjolson@gmail.com>
2019-09-09 14:15:39 -0500

    conditionally rendering denial message on NPI view

--- src/components/NpiView/NpiView.js
+++ src/components/NpiView/NpiView.js
@@ -126,6 +126,16 @@ const styles = theme => ({
         borderRight: '1px solid black',
         borderBottom: '1px solid black',
     },
+    denialmessage: {
+        textAlign: 'center',
+        width: '80%',
+        marginTop: '10px',
+        margin: 'auto',
+        backgroundColor: 'white',
+        fontSize: '1.2em',
+        padding: '3px',
+        border: '1px solid red',
+    },
 });
 
 function getModalStyle() {
@@ -172,6 +182,18 @@ class NpiView extends Component {
         this.props.dispatch({ type: 'FETCH_PCN_INFO', payload: data });
         this.props.dispatch({ type: 'FETCH_PCN_PARTS', payload: data });
         this.props.dispatch({ type: 'FETCH_PCN_IMAGES', payload: data });
+    }
+
+    checkMessage() {
+        const { classes } = this.props;
+        if (this.props.reduxStore.pcnInfo.status === 'DENIED') {
+            return (
+                <div className={classes.denialmessage}>
+                    <h4> Admin Notes </h4>
+                    <p>{this.props.reduxStore.pcnInfo.notification_message}</p>
+                </div>
+            )
+        }
     }
 
     handleOpen = () => {
@@ -237,9 +259,8 @@ class NpiView extends Component {
                 <div className={classes.pcnbackground}>
                     <Nav history={this.props.history} />
                     <div className={classes.pcnform}>
+                            {this.checkMessage()}
                         <div className={classes.pcnheader}>
-                            {/* <h2>Company Name Here</h2> 
-                        <h2>Product Change Notification</h2> */}
                             <h2>{this.props.reduxStore.pcnInfo.id}</h2>
                             <div className={classes.pcndate}>
                                 <p>{Moment(this.props.reduxStore.pcnInfo.date).format('MM/DD/YYYY')}</p>
